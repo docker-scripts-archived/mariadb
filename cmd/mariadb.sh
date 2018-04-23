4ccf4cd36f8728f30c53a06e775c1851a72fdf7e
dashohoxha <dashohoxha@gmail.com>
2018-04-23 07:18:28 +0200

    Commands and scripts to backup and restore databases.

--- cmd/mariadb.sh
+++ cmd/mariadb.sh
@@ -8,6 +8,10 @@ cmd_mariadb_help() {
               drop the database '$DBNAME'
         - dump
               dump the database '$DBNAME'
+        - backup [<dbname>...]
+              backup the given databases (default '$DBNAME')
+        - restore <backup-file.tgz>
+              restore databases from the given backup file
         - sql <query>
               run the given sql query on '$DBNAME'
         - script <file.sql>
@@ -20,10 +24,11 @@ cmd_mariadb() {
     [[ -n $DBHOST ]] || fail "Error: No DBHOST defined on 'settings.sh'"
 
     local cmd=$1
+    shift
     case $cmd in
         sql)
-            [[ -n $2 ]] || fail "Usage:\n$(cmd_mariadb_help)"
-            ds @$DBHOST exec mysql -e "$2"
+            [[ -n $1 ]] || fail "Usage:\n$(cmd_mariadb_help)"
+            ds @$DBHOST exec mysql -e "$1"
             ;;
         drop)
             ds @$DBHOST exec mysql -e "drop database if exists $DBNAME"
@@ -39,9 +44,31 @@ cmd_mariadb() {
         dump)
             ds @$DBHOST exec mysqldump --allow-keywords --opt $DBNAME
             ;;
+        backup)
+            # get the backup name
+            local databases="$@"
+            databases=${databases:-$DBNAME}
+            local backup="$DBHOST-${databases// /-}-$(date +%Y%m%d)"
+
+            # make the backup file
+            ds @$DBHOST inject backup.sh $backup $databases
+
+            # move the backup file to the current directory
+            mv /var/ds/$DBHOST/$backup.tgz .
+            ;;
+        restore)
+            # get the backup file
+            local file=$1
+            [[ -f $file ]] || fail "Usage:\n$(cmd_mariadb_help)"
+            cp -f $file /var/ds/$DBHOST/
+
+            # restore databases from the backup file
+            ds @$DBHOST inject restore.sh $(basename $file)
+            rm -f /var/ds/$DBHOST/$(basename $file)
+            ;;
         script)
-            [[ -n $2 ]] || fail "Usage:\n$(cmd_mariadb_help)"
-            sqlfile=$2
+            [[ -n $1 ]] || fail "Usage:\n$(cmd_mariadb_help)"
+            sqlfile=$1
             ds @$DBHOST exec sh -c "mysql --database=$DBNAME < /host/$sqlfile"
             ;;
         *)
---
+++ scripts/restore.sh
@@ -0,0 +1,38 @@
+#!/bin/bash -x
+
+# get the name of the backup dir
+file=$1
+backup=${file%%.tgz}
+backup=$(basename $backup)
+
+# extract the archive
+cd /host/
+tar --extract --gunzip --file $backup.tgz
+mariabackup \
+    --defaults-file=/etc/mysql/debian.cnf \
+    --prepare \
+    --target-dir=$backup
+
+# remove the databases that are going to be restored
+cd $backup
+for dir in $(ls -d */); do
+    dbname=${dir%/}
+    mysql -e "drop database $dbname";
+done
+cd ..
+
+# restore
+# See: https://mariadb.com/kb/en/library/full-backup-and-restore-with-mariadb-backup/#restoring-with-other-tools
+systemctl stop mariadb.service
+
+rsync -avrP $backup/ /var/lib/mysql/
+rm -f /var/lib/mysql/ib_logfile*
+
+chown -R mysql:mysql /var/lib/mysql/
+chmod g+rw -R /var/lib/mysql/
+
+systemctl start mariadb.service
+
+# clean up
+rm -rf $backup
+rm -f $backup.tgz
